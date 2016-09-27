1c5860397f260390bcba517af1cc5210c12ac205
WZnRa <oswaldzinc@gmail.com>
2016-09-28 06:10:38 +0800

    +

--- ex35.py
+++ ex35.py
@@ -20,15 +20,16 @@ def bear_room():
     print "The bear has a bunch of money."
     print "The fat bear is in front of another door."
     print "How are you going to move the bear?"
+    bear_moved = False
 
     while True:
         next = raw_input(">")
         if next == "take money":
             dead("The bear looks at you then slaps your face off.")
-        elif next == "taunt bear" and not bear_moved
+        elif next == "taunt bear" and not bear_moved:
             print "The bear has moved from the door. You can go through it now."
             bear_moved = True
-        elif next == "taunt bear and bear_moved"
+        elif next == "taunt bear and bear_moved":
             dead ("The gear gets pissed off and chews your leg off.")
         elif next == "open door" and bear_moved:
             gold_room()
@@ -39,4 +40,39 @@ def cthulhu_room():
     print "Here you see the great evil Cthulhu."
     print "He, it, whatever stares at you and you go insane."
     print "Do you flee for your life or eat you head?"
-    
+
+    next  = raw.input(">")
+
+    if "flee" in next:
+        start()
+    elif "head" in next:
+        dead("Well that was tasty!")
+    else:
+        cthulhu_room()
+
+def dead(why):
+    print why, "Good Job!"
+    exit(0)
+
+def start():
+    print "You are in a dark room."
+    print "There is a door to your right and left."
+    print "Which one do you take?"
+
+    next = raw_input(">")
+
+    if next == "left":
+        bear_room()
+    elif next == "right":
+        cthlhu_room()
+    else:
+        dead("You stumble around the room until you starve.")
+
+start()
+
+
+
+
+
+
+
