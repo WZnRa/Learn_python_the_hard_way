6deb50424ff3de69019cc5e0b25515eae372d70d
WZnRa <oswaldzinc@gmail.com>
2016-10-02 01:17:33 +0800

    +

--- ex432.py
+++ ex432.py
@@ -35,7 +35,34 @@ class Death(Scene):
         
 class CentralCorridor(Scene):
     print "The Gothons of Planet Percal #25 have invaded your ship and destrotyed"
-    print ""
+    print "your entire crew. You are the last surviving member and your last"
+    print "mission is to get the neutron destruct bomb from the Weapons Armory."
+    print "put it in the bridge, and blow the ship up after getting into an "
+    print "escape pod."
+    print "\n"
+    print "You're running down the central corridor the the Weapon Armory when"
+    print "a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume"
+    print "flowing around his hate filled body. He's blocking the door to the"
+    print "Armory and about to pull a weapon to blast you."
+    
+    action = raw.input("> ")
+    
+    if action == "Shoot!":
+        print "Quick on the draw you yank out your blaster and fire it at the Gothon."
+        print "His clown costume is flowing and moving around his body, which throws"
+        print "off your aim. Your laser hits his costume but misses him entirely. This"
+        print "completely ruins his brand new costue his mother bought him, which"
+        print "makes him fly into a rage and blast you repeatedly in the face until"
+        print "you are dead. Then he eats you."
+        return 'death'
+        
+    elif action == "dodge!":
+        print "Like a world class boxer you dodge, weave, slip and slide right"
+        print "as the Gothon's blaster cranks a laser past your head."
+        print "In the middle of your artful dodge your foot slips and you"
+        print "bang your head on the metal wall and pass out."
+        print "You wake up shortly after only to die as the Gothon stomps on"
+        print "your "
     
     def enter(self):
         pass
